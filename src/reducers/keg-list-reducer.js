e7552ab6eb24c848e8b20693f4c512fdf4e8f83f
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-11 23:09:35 -0400

    Add readme content

---
+++ src/reducers/keg-list-reducer.js
@@ -0,0 +1,17 @@
+export default (state = {}, action) => {
+  const { name, brand, price, alcoholeContent, id } = action;
+  switch (action.type) {
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        names: name,
+        brand: brand,
+        price: price,
+        alcoholeContent: alcoholeContent,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
+};
